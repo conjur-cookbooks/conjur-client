1e9827d4693ad1a381adbcc1614d218b3619dbe2
Kevin Gilpin <kgilpin@gmail.com>
2014-03-19 13:49:56 -0400

    test conjur.bootstrap-bucket attribute

--- spec/bootstrap_spec.rb
+++ spec/bootstrap_spec.rb
@@ -1,16 +1,7 @@
 require 'spec_helper'
 
 describe 'conjur-client::bootstrap' do
-  # http://stackoverflow.com/questions/625644/find-out-the-instance-id-from-within-an-ec2-machine/21944204#21944204
-  let(:instance_document) {
-    {
-      "accountId" => "12345678"
-    }
-  }
-  before {
-    require 'uri'
-    URI.should_receive(:parse).with("http://169.254.169.254/latest/dynamic/instance-identity/document").and_return double("instance-document", read: instance_document.to_json)
-  }
+  let(:bucket) { nil }
   let(:conjurrc) {
     {
       "account" => 'the-account'
@@ -19,20 +10,42 @@ describe 'conjur-client::bootstrap' do
   let :chef_run do
     ChefSpec::Runner.new do |node|
       node.set['conjur']['configuration']['account'] = 'the-account'
+      node.set['conjur']['bootstrap-bucket'] = bucket if bucket
       node.set['platform_family'] = platform_family if platform_family
     end.converge described_recipe
   end
   let(:platform_family) { "debian" }
-  it 'installs aws-sdk into Chef' do
-    chef_run.should install_gem_package('aws-sdk').with(gem_binary: "/opt/conjur/embedded/bin/gem")
+  context "with default bucket" do
+    # http://stackoverflow.com/questions/625644/find-out-the-instance-id-from-within-an-ec2-machine/21944204#21944204
+    let(:instance_document) {
+      {
+        "accountId" => "12345678"
+      }
+    }
+    before {
+      require 'uri'
+      URI.should_receive(:parse).with("http://169.254.169.254/latest/dynamic/instance-identity/document").and_return double("instance-document", read: instance_document.to_json)
+    }
+    it 'installs aws-sdk into Chef' do
+      chef_run.should install_gem_package('aws-sdk').with(gem_binary: "/opt/conjur/embedded/bin/gem")
+    end
+    it 'creates /etc/conjur-bootstrap-bucket' do
+      chef_run.should create_file('/etc/conjur-bootstrap-bucket').with(
+        content: "conjur-12345678-bootstrap",
+        mode: '0644'
+      )
+    end
+    it 'creates /etc/conjur-bootstrap.conf' do
+      chef_run.should create_template('/etc/init/conjur-bootstrap.conf').with(variables: { start_event: "(local-filesystems and net-device-up IFACE!=lo)"})
+    end
   end
-  it 'creates /etc/conjur-bootstrap-bucket' do
-    chef_run.should create_file('/etc/conjur-bootstrap-bucket').with(
-      content: "conjur-12345678-bootstrap",
-      mode: '0644'
-    )
-  end
-  it 'creates /etc/conjur-bootstrap.conf' do
-    chef_run.should create_template('/etc/init/conjur-bootstrap.conf').with(variables: { start_event: "(local-filesystems and net-device-up IFACE!=lo)"})
+  context "with specified bucket" do
+    let(:bucket) { "ham" }
+    it 'creates /etc/conjur-bootstrap-bucket' do
+      chef_run.should create_file('/etc/conjur-bootstrap-bucket').with(
+        content: "ham",
+        mode: '0644'
+      )
+    end
   end
 end
