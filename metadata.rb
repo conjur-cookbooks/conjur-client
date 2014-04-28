f2f9548f57f75c630d79b3bd5499cd8ced75b994
Kevin Gilpin <kgilpin@gmail.com>
2014-04-28 00:12:25 -0400

    fix cookbook name

--- metadata.rb
+++ metadata.rb
@@ -1,14 +1,14 @@
-name              'conjur-cli'
+name              'conjur-client'
 maintainer        'Conjur, Inc.'
 maintainer_email  'kgilpin@conjur.net'
 license           'MIT'
 description       'Installs the Conjur command line interface'
 version           '0.3.0'
 
-recipe "conjur-cli::bootstrap", "Installs an upstart script which will fetch solo.json from an S3 bucket and run chef-solo"
-recipe "conjur-cli::conjurrc", "Creates /etc/conjur.conf"
-recipe "conjur-cli::default", "Installs the Conjur client"
-recipe "conjur-cli::ssl_certificate", "Installs the Conjur virtual appliance SSL certificate"
+recipe "conjur-client::bootstrap", "Installs an upstart script which will fetch solo.json from an S3 bucket and run chef-solo"
+recipe "conjur-client::conjurrc", "Creates /etc/conjur.conf"
+recipe "conjur-client::default", "Installs the Conjur client"
+recipe "conjur-client::ssl_certificate", "Installs the Conjur virtual appliance SSL certificate"
 
 depends "build-essential"
 
